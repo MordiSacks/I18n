5374d0db50a1c35fb4a51ed121bb0777dce7d81d
Mordi Sacks <mordisacks@gmail.com>
2016-06-13 21:54:04 +0300

    added production mode

--- src/I18n.php
+++ src/I18n.php
@@ -8,6 +8,8 @@ class I18n
 {
 	/** @var  string Lang directory */
 	protected static $dir = null;
+	/** @var  boolean|bool Production mode */
+	protected static $production = true;
 	/** @var  string Current locale */
 	protected static $locale = null;
 	/** @var array Loaded locales */
@@ -31,6 +33,25 @@ class I18n
 	{
 		self::$dir = $dir;
 	}
+
+	/**
+	 * @return boolean
+	 */
+	public static function isProduction()
+	{
+		return self::$production;
+	}
+
+	/**
+	 * If is set to false, we will create all missing files and strings
+	 *
+	 * @param boolean $production
+	 */
+	public static function setProduction($production)
+	{
+		self::$production = $production;
+	}
+
 
 	/**
 	 * Set locale
@@ -59,20 +80,96 @@ class I18n
 	 */
 	public static function translate($string, $text_domain = 'default')
 	{
+		/** @var string $file locale file path */
 		$file = static::$dir . DIRECTORY_SEPARATOR . static::$locale . DIRECTORY_SEPARATOR . $text_domain . '.php';
 
+		/**
+		 * Check if we already loaded this file
+		 */
 		if(array_key_exists($file, self::$loaded))
 		{
-			return isset(self::$loaded[$file][$string]) ? self::$loaded[$file][$string] : $string;
+			/**
+			 * Save missing string if not in production
+			 */
+			if(!self::isProduction() && (!isset(self::$loaded[$file][$string])))
+			{
+				return self::addTranslation($file, $string);
+			}
+
+			/** return processed string */
+			return isset(self::$loaded[$file][$string]) && !empty(self::$loaded[$file][$string]) ? self::$loaded[$file][$string] : $string;
 		}
 
+		/**
+		 * Check if the file exists
+		 */
 		if(!file_exists($file))
 		{
+			/**
+			 * Create file if not in production
+			 */
+			if(!self::isProduction())
+			{
+				self::saveTranslationFile($file, [$string => '']);
+			}
+
 			return $string;
 		}
 		/** @noinspection PhpIncludeInspection */
 		self::$loaded[$file] = (array)require $file;
 
-		return isset(self::$loaded[$file][$string]) ? self::$loaded[$file][$string] : $string;
+		/**
+		 * Save missing string if not in production
+		 */
+		if(!self::isProduction() && (!isset(self::$loaded[$file][$string])))
+		{
+			return self::addTranslation($file, $string);
+		}
+
+		/** return processed string */
+		return isset(self::$loaded[$file][$string]) && !empty(self::$loaded[$file][$string]) ? self::$loaded[$file][$string] : $string;
+	}
+
+	/**
+	 * @param $file
+	 * @param $strings
+	 */
+	protected static function saveTranslationFile($file, $strings)
+	{
+		file_put_contents($file, "<?php\n/** I18n Auto generated file on " . date('Y-m-d H:i:s') . " */\nreturn " . self::TranslationArrayExport($strings));
+	}
+
+	/**
+	 * Like var_export, but pretty
+	 *
+	 * @param array $array I18n strings
+	 *
+	 * @return string
+	 */
+	protected static function TranslationArrayExport($array)
+	{
+		$output = "[\n";
+		foreach($array as $key => $value)
+		{
+			$output .= "\t'{$key}' => '{$value}',\n";
+		}
+
+		return $output . "];";
+	}
+
+	/**
+	 * Add missing string to an existing locale
+	 *
+	 * @param string $file
+	 * @param string $string
+	 *
+	 * @return string
+	 */
+	protected static function addTranslation($file, $string)
+	{
+		self::$loaded[$file] = array_merge(self::$loaded[$file], [$string => '']);
+		self::saveTranslationFile($file, self::$loaded[$file]);
+
+		return $string;
 	}
 }
